672fffe467f21bc71943bd84eaad84b9f06dd18e
Heitor Kunrath <60748906+HeitorKun@users.noreply.github.com>
2024-04-15 12:04:01 -0300

    doing 3

--- main.sql
+++ main.sql
@@ -1,87 +1,3 @@
-select 'create table '
-       || table_name
-       || ' as select * from ARRUDA.'
-       || table_name
-       || ';'
-  from all_tables
- where owner = 'ARRUDA'
-   and table_name like 'AIR_%';
-
-create table air_airlines
-	as
-		select *
-		  from arruda.air_airlines;
-
-create table air_airplanes
-	as
-		select *
-		  from arruda.air_airplanes;
-
-create table air_airplane_types
-	as
-		select *
-		  from arruda.air_airplane_types;
-
-create table air_airports
-	as
-		select *
-		  from arruda.air_airports;
-
-create table air_airports_geo
-	as
-		select *
-		  from arruda.air_airports_geo;
-
-create table air_bookings
-	as
-		select *
-		  from arruda.air_bookings;
-
-create table air_flights
-	as
-		select *
-		  from arruda.air_flights;
-
-create table air_flights_schedules
-	as
-		select *
-		  from arruda.air_flights_schedules;
-
-create table air_passengers
-	as
-		select *
-		  from arruda.air_passengers;
-
-create table air_passengers_details
-	as
-		select *
-		  from arruda.air_passengers_details;
-
-
-
-
-
-
---3
-
-SELECT 
-    AF.FLIGHTNO AS FlightNumber,
-    DEP_AIRPORT.NAME AS DepartureAirport,
-    ARR_AIRPORT.NAME AS ArrivalAirport,
-    (AP.FIRSTNAME || ' ' || AP.LASTNAME) AS FullName,
-    AB.SEAT AS SeatNumber
-FROM 
-    AIR_FLIGHTS AF
-INNER JOIN 
-    AIR_AIRPORTS DEP_AIRPORT ON AF.FROM_AIRPORT_ID = DEP_AIRPORT.AIRPORT_ID
-INNER JOIN 
-    AIR_AIRPORTS ARR_AIRPORT ON AF.TO_AIRPORT_ID = ARR_AIRPORT.AIRPORT_ID
-INNER JOIN 
-    AIR_BOOKINGS AB ON AF.FLIGHT_ID = AB.FLIGHT_ID
-INNER JOIN 
-    AIR_PASSENGERS AP ON AB.PASSENGER_ID = AP.PASSENGER_ID
-WHERE 
-    TRUNC(AF.DEPARTURE) = DATE '2023-03-25';
 
 
 --4
@@ -121,7 +37,9 @@ FROM
 INNER JOIN 
     AIR_PASSENGERS_DETAILS APD ON AP.PASSENGER_ID = APD.PASSENGER_ID
 INNER JOIN 
-    AIR_BOOKINGS AB ON AP.PASSENGER_ID = AB.PASSENGER_ID;
+    AIR_BOOKINGS AB ON AP.PASSENGER_ID = AB.PASSENGER_ID
+WHERE 
+    APD.COUNTRY = 'BRAZIL' OR APD.COUNTRY = 'USA';
 
 
 
@@ -235,4 +153,25 @@ AS SELECT * FROM DSILVA.AIR_AIRPORTS;
 
 
 
--- 3 
+
+--3
+
+SELECT 
+    AF.FLIGHTNO AS FlightNumber,
+    DEP_AIRPORT.NAME AS DepartureAirport,
+    ARR_AIRPORT.NAME AS ArrivalAirport,
+    (AP.FIRSTNAME || ' ' || AP.LASTNAME) AS FullName,
+    AB.SEAT AS SeatNumber
+FROM 
+    AIR_FLIGHTS AF
+INNER JOIN 
+    AIR_AIRPORTS DEP_AIRPORT ON AF.FROM_AIRPORT_ID = DEP_AIRPORT.AIRPORT_ID
+INNER JOIN 
+    AIR_AIRPORTS ARR_AIRPORT ON AF.TO_AIRPORT_ID = ARR_AIRPORT.AIRPORT_ID
+INNER JOIN 
+    AIR_BOOKINGS AB ON AF.FLIGHT_ID = AB.FLIGHT_ID
+INNER JOIN 
+    AIR_PASSENGERS AP ON AB.PASSENGER_ID = AP.PASSENGER_ID
+WHERE 
+    TRUNC(AF.DEPARTURE) = DATE '2023-03-25';
+
